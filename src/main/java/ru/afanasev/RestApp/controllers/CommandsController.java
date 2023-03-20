5ed7a7ad94cac78d730ed0f9dd9725fbdd81f041
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2023-03-20 09:43:05 +0700

    Уже можем получить все команды

--- src/main/java/ru/afanasev/RestApp/controllers/CommandsController.java
+++ src/main/java/ru/afanasev/RestApp/controllers/CommandsController.java
@@ -1,12 +1,27 @@
 package ru.afanasev.RestApp.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.afanasev.RestApp.models.Command;
+import ru.afanasev.RestApp.services.CommandsService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/commands")
 public class CommandsController {
 
+    private final CommandsService commandsService;
 
+    @Autowired
+    public CommandsController(CommandsService commandsService) {
+        this.commandsService = commandsService;
+    }
+
+    public List<Command> getCommands() {
+        return commandsService.findAll();
+
+    }
 }
